c5be2c1950a2124c24ce507c91177e92cfe34605
Dejan Fajfar <dejan@fajfar.com>
2019-05-21 22:36:03 +0200

    Introduced scss for styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,26 @@
+'use strict';
+
+const sass = require('node-sass');
+
 module.exports = function(grunt) {
     grunt.initConfig({
         copy: {
             dist: {
                 files: [
-                    {src: 'src/index.css', dest: 'dist/index.css'},
                     {src: 'src/index.html', dest: 'dist/index.html'},
                     {expand: true, src: 'assets/images/**', dest: 'dist/images/', filter: 'isFile', flatten: true}
                 ]
+            }
+        },
+        sass: {
+            options: {
+                implementation: sass,
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'dist/index.css': 'src/styles/main.scss'
+                }
             }
         },
         clean: {
@@ -16,6 +30,7 @@ module.exports = function(grunt) {
     
         grunt.loadNpmTasks('grunt-contrib-copy');
         grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-sass');
     
-    grunt.registerTask('default', ['clean:dist', 'copy:dist']);
+    grunt.registerTask('default', ['clean:dist', 'copy:dist', 'sass']);
 };
